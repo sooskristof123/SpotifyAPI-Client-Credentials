b65a9b7dc5774cb654cd6e1f75496893a89ad91a
sooskristof123 <sooskristof123@gmail.com>
2021-10-13 11:05:47 +0200

    c

---
+++ WebApplication5/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication5.Services;
+
+namespace WebApplication5
+{
+    public class HomeController : Controller
+    {
+        private readonly ISpotifyAccountServices iSpotifyAccountServices;
+        private readonly IConfiguration iConfiguration;
+        public HomeController(ISpotifyAccountServices iSpotifyAccountServices, IConfiguration iConfiguration) {
+            this.iSpotifyAccountServices = iSpotifyAccountServices;
+            this.iConfiguration = iConfiguration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var token = await iSpotifyAccountServices.GetToken(iConfiguration["Spotify:clientId"], iConfiguration["Spotify:clientSecret"]);
+                
+            }
+
+            catch (Exception ex)
+            {
+                Debug.Write(ex);
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication5/WebApplication5/Services/SpotifyAccountServices.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApplication5.Models;
+
+namespace WebApplication5.Services
+{
+    public class SpotifyAccountServices : ISpotifyAccountServices
+    { 
+        private readonly HttpClient httpClient;
+        public SpotifyAccountServices(HttpClient httpClient) {
+            this.httpClient = httpClient;
+        }
+        public async Task<string> GetToken(string clientId, string clientSecret)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "token");
+            request.Headers.Authorization = new AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"))
+            );
+
+            request.Content = new FormUrlEncodedContent( new Dictionary<string, string> {
+                {"grant_type", "client_credentials"}
+            });
+            var respone = await httpClient.SendAsync(request);
+            respone.EnsureSuccessStatusCode();
+
+            using var responseStream = await respone.Content.ReadAsStreamAsync();
+            var authResult = await JsonSerializer.DeserializeAsync<AuthToken>(responseStream);
+
+
+            return authResult.access_token;
+        }
+    }
+}
